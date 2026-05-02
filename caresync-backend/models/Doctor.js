40e44c1c93f92bd9d8c65de471e18fd0175fc50b
RisiMohamed <159991149+RisiMohamed@users.noreply.github.com>
2026-05-02 18:32:11 +0530

    Doctor Model Completed

--- caresync-backend/models/Doctor.js
+++ caresync-backend/models/Doctor.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 const doctorSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
   specialisation: { type: String, required: true },
